d48871de1e845cc4c3fc8b1f4ef5606ee04f9621
Felipe <felipefranca824@gmail.com>
2021-11-29 16:36:01 -0300

    Criado os testes para a model

---
+++ lib/features/data/datasources/space_media_datasource_implementation.dart
@@ -0,0 +1,10 @@
+import 'package:nasa_media/features/data/datasources/space_media_datasource.dart';
+import 'package:nasa_media/features/data/models/space_media_model.dart';
+
+class SpaceMediaDatasouceImplementation implements ISpaceMediaDatasource {
+  @override
+  Future<SpaceMediaModel> getSpaceMediaFromDate(DateTime date) {
+    // TODO: implement getSpaceMediaFromDate
+    throw UnimplementedError();
+  }
+}
